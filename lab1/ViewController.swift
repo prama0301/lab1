4ced426d0e7fba741af156f38e7c91ea27ea0ac5
Sayali <18102056@apsit.edu.in>
2022-11-22 05:38:42 -0800

    NewFiles

--- lab1/ViewController.swift
+++ lab1/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
@@ -14,6 +16,25 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInBtn(_ sender: Any) {
+        let auth = Auth.auth()
+        auth.signInAnonymously { (result, err) in
+            if let err = err {
+                print(err.localizedDescription)
+                return
+            }
+            print("sucessfull login")
+        }
+    }
+    
+    @IBAction func signOut(_ sender: Any) {
+        let auth = Auth.auth()
+        do {
+            try auth.signOut()
+            print("sucessfull logout")
+        } catch (let err) {
+            print(err.localizedDescription)
+        }
+    }
 }
 
