a1c3bdbe81db97f51224618ecf3d0bb32473fb2f
Sayali <18102056@apsit.edu.in>
2022-11-21 20:22:17 -0800

    Initial Commit

---
+++ lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab1
+//
+//  Created by Capgemini-DA071 on 11/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
